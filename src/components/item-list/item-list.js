6c2909175a435813c5027295deb381a04938fb5f
VPolazau <VPolazau@gmail.com>
2022-12-12 22:49:54 +0300

    add pagination

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,35 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import Pagination from '@mui/material/Pagination'
+import Stack from '@mui/material/Stack'
 import { getAllProducts, getProductsByCategory } from '../../service/shop-service'
 import { btnEvent } from '../events/event'
+import Item from '../item/item'
 
 import './item-list.css'
 
 const ItemList = () => {
   const products = useSelector(store => store.storeData.products)
-  const [items, setItems] = useState(products)
   const [page, setPage] = useState(1)
   const [category, setCategory] = useState('all') // w
   const dispatch = useDispatch()
 
   useEffect(() => {
-    if(category == 'all'){
-      getAllProducts(0, 20, dispatch)
+    if (category == 'all') {
+      getAllProducts(0, dispatch)
+      setPage(1)
     }
     getProductsByCategory(category, dispatch)
   }, [category])
 
   useEffect(() => {
-    getAllProducts(page * 20 - 20, page * 20, dispatch)
+    getAllProducts(page * 20 - 20, dispatch)
   }, [page])
 
   useEffect(() => {
     btnEvent.addListener('onChangeCategory', c => setCategory(c))
   }, [])
 
-  
+  const handleChange = (event, value) => {
+    setPage(value)
+  }
 
-  return <div className='ItemList'>ItemList</div>
+  console.log(products)
+  return (
+    <div className='ItemList'>
+      <div className='list'>
+        {products.map(item => (
+          <Item key={item.id} id={item.id} info={item.body} />
+        ))}
+      </div>
+      {category === 'all' ? (
+        <Stack spacing={2} sx={{ marginLeft: 'auto', marginRight: '5%' }}>
+          <Pagination count={5} onChange={handleChange} href='#top' page={page} />
+        </Stack>
+      ) : null}
+    </div>
+  )
 }
 
 export default ItemList
